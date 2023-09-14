e1f0c206f44f09f2d24ec4fc41480e5ced036a22
bobjoerules <yo.bob.joe.rules@gmail.com>
2023-09-14 13:24:40 -0700

    cleaning

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
 function searchSite() {
-    var input, filter, ul, li, a, i, txtValue;
-    input = document.getElementById('searchapp');
-    filter = input.value.toUpperCase();
-    ul1 = document.getElementById("all");
-    li1 = ul1.getElementsByTagName('li');
-  
-    // Loop through all list items, and hide those who don't match the search query
-    for (i = 0; i < li1.length; i++) {
-      a = li1[i].getElementsByTagName("a")[0];
-      txtValue = a.textContent || a.innerText;
-      if (txtValue.toUpperCase().startsWith(filter)) {
-        li1[i].style.display = "";
-      } else {
-        li1[i].style.display = "none";
-      }
+  var input, filter, ul, li, a, i, txtValue;
+  input = document.getElementById('searchapp');
+  filter = input.value.toUpperCase();
+  ul1 = document.getElementById("all");
+  li1 = ul1.getElementsByTagName('li');
+  for (i = 0; i < li1.length; i++) {
+    a = li1[i].getElementsByTagName("a")[0];
+    txtValue = a.textContent || a.innerText;
+    if (txtValue.toUpperCase().startsWith(filter)) {
+      li1[i].style.display = "";
+    } else {
+      li1[i].style.display = "none";
     }
   }
-  
+}
+const allElements = document.querySelectorAll('*');
+allElements.forEach(element => {
+    element.draggable = false;
+});
