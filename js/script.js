048490b8c4ab15ae3c3ccf343b51642e96cb7356
bobjoerules <yo.bob.joe.rules@gmail.com>
2023-11-09 13:50:45 -0800

    type of software detection

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
 function searchSite() {
-  var input, filter, ul, li, a, i, txtValue;
+  var input, filter, ul, li, a, i, number, txtValue;
   input = document.getElementById('searchapp');
   filter = input.value.toUpperCase();
+  number = 0
   ul1 = document.getElementById("all");
   li1 = ul1.getElementsByTagName('li');
   for (i = 0; i < li1.length; i++) {
@@ -9,9 +10,15 @@ function searchSite() {
     txtValue = a.textContent || a.innerText;
     if (txtValue.toUpperCase().startsWith(filter)) {
       li1[i].style.display = "";
+      number = 1;
     } else {
       li1[i].style.display = "none";
     }
+  }
+  if (number === 0) {
+    document.getElementById('nothingfound').style.display = "";
+  } else {
+    document.getElementById('nothingfound').style.display = "none";
   }
 }
 const allElements = document.querySelectorAll('*');
@@ -30,3 +37,30 @@ async function start() {
   await delay(10000);
 }
 start()
+var is_OSX = /(Mac|iPhone|iPod|iPad)/i.test(navigator.platform);
+var is_iOS = /(iPhone|iPod|iPad)/i.test(navigator.platform);
+
+var is_Mac = navigator.platform.toUpperCase().indexOf('MAC') >= 0;
+console.log(navigator.platform)
+var is_windows = navigator.platform.indexOf('Win') >=0
+var is_iPhone = navigator.platform == "iPhone";
+var is_iPod = navigator.platform == "iPod";
+var is_iPad = navigator.platform == "iPad";
+var is_Android = navigator.platform == "Android";
+
+var mac = document.getElementById('mac');
+var windows = document.getElementById('windows');
+var iphone = document.getElementById('iphone');
+var android = document.getElementById('android');
+
+/* if (!is_OSX) type.innerHTML += "This NOT a Mac or an iOS Device!";
+if (is_Mac) type.innerHTML += "This is a Mac Computer!\n";
+if (is_iOS) type.innerHTML += "You're using an iOS Device!\n";
+if (is_iPhone) type.innerHTML += "This is an iPhone!";
+if (is_iPod) type.innerHTML += "This is an iPod Touch!";
+if (is_iPad) type.innerHTML += "This is an iPad!";*/
+
+if (!is_iPhone && !is_windows && !is_Android) windows.innerHTML += "Your computer is Not A Windows Computer so some of these programs might not work with DarkMode on your device";
+if (!is_iPhone && !is_Mac && !is_Android) mac.innerHTML += "Your computer is Not A Mac Computer so some of these programs might not work with DarkMode on your device";
+if (!is_iPhone && !is_Mac && !is_windows) iphone.innerHTML += "Your Phone is Not An iPhone so some of these programs might not work with DarkMode on your device";
+if (!is_Android && !is_Mac && !is_windows) android.innerHTML += "Your Phone/Tablet is Not An Android so some of these programs might not work with DarkMode on your device";
